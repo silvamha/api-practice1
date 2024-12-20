04de7c41803405fd64c1a685c7a6f7fff5d8b222
Ed <silva.email@yahoo.com>
2024-12-20 05:31:40 -0500

    updated main

--- main.js
+++ main.js
@@ -1,9 +1,77 @@
-const countryName = 'Spain'
+const countryName = "Spain";
 
-async function getData(){
-    const url = `https://restcountries.com/v3.1/capital/${countryName}`
-    const response = await fetch(url)
-    console.log(response.name)
+// async function getData(){
+//     const url = ``
+//     const response = await fetch(url)
+//     console.log(response.name)
+// }
+
+// getData()
+
+const exampleArray = [
+  {
+    yourFirstName: "John",
+    yourLastName: "Doe",
+    yourAge: 21,
+    isCitizen: true,
+  },
+  {
+    yourFirstName: "Mary",
+    yourLastName: "Jane",
+    yourAge: 29,
+    isCitizen: false,
+  },
+  {
+    yourFirstName: "Karl",
+    yourLastName: "Marxx",
+    yourAge: 43,
+    isCitizen: false,
+  },
+];
+
+// for(let example in exampleArray){
+// console.log(example)
+// }
+
+// console.log(exampleArray)
+
+fetch("https://restcountries.com/v3.1/name/france")
+  .then((response) => {
+    return response.json();
+  })
+  .then((countryData) => {
+    const country = countryData[0];
+    const countryName = country.name.common;
+    const capital = country.capital[0];
+
+    console.log(country);
+    console.log(countryName);
+    console.log(`The capital of ${countryName} is ${capital}`);
+
+    const containerEl = document.querySelector("#container-el");
+
+    containerEl.innerHTML = `
+    <div>
+    <h1>Render Data</h1>
+    <p>The capital of <strong>${countryName} is ${capital}</strong> </p>
+    </div>
+    `;
+  });
+
+async function getCountryData() {
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/name/brazil`);
+    const countryData = await response.json();
+
+    const country = countryData[0];
+    const countryName = country.name.common;
+    const capital = country.capital[0];
+
+    console.log(
+      `The country name is ${countryName} and the capital is ${capital}`
+    );
+  } catch (error) {
+    console.error("Error fetching or processing data: ", error);
+  }
 }
-
-getData()
+getCountryData();
